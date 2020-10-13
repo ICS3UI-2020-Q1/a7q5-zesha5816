138b7e6d8d32861a4357f0c85bd19e53fe638b94
zesha5816 <zesha5816@wrdsb.ca>
2020-10-13 15:59:25 +0000

    Finished example

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 /**
- *
- * @author 
+ * a method named firstDigit that returns the first digit of an integer.
+ * @author Ahmad Zeshan 
  */
 public class Main {
 
+	public static int firstDigit(int num){
+		// repeat as long as num is 2 or more digits
+    while (num > 10 || num < -10){
+      // keep dividing num by 10
+      num = num/10;
+    }
+    // this will return the first digit to the screen
+    return num;
+	}
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    int finalAnswer = firstDigit(-643456);
+		System.out.println(finalAnswer);
     
   }
 }
